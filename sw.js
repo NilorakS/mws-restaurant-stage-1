68031a87a4ccbcbb9b0f60a5f93f32cd1af0f48f
NilorakS <karo.striegel@gmail.com>
2018-11-18 18:28:45 +0100

    feat: Add activate listener to service worker

--- sw.js
+++ sw.js
@@ -22,10 +22,26 @@ const cacheUrls = [
 	'/img/favicon.ico'
 ];
 
+// listen for the browser to set up a new service worker and cache urls
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache) {
 			return cache.addAll(cacheUrls);
 		})
 	);
+});
+
+// listen for a service worker to become active and clear old caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('rev-') && cacheName !== staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
